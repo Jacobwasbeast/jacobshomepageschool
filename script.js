e90377f20353ac8847967f6fb129f97d23418fa7
Jacobwasbeast <38381609+Jacobwasbeast@users.noreply.github.com>
2019-04-29 10:35:30 -0500

    Website

---
+++ script.js
@@ -0,0 +1,231 @@
+//Question 1#
+function askQuestionP1() {
+  var userAnswer = prompt ("Air, water, and land") ;
+     if (userAnswer == "3 Diffrent Types of Polution") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionE1() {
+  var userAnswer = prompt ("When a animal or other living things migrate to another Ecosytem") ;
+   if (userAnswer == "Migration") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+
+function askQuestionCe1() {
+  var userAnswer = prompt ("Carry on all life processes") ;
+     if (userAnswer == "Cells") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionEn1() {
+  var userAnswer = prompt ("energy which a body possesses by virtue of being in motion") ;
+  if (userAnswer == "Kinetic energy") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionC1() {
+  var userAnswer = prompt ("Type of community that has the most open land") ;
+  if (userAnswer == "What is a rural community?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+//Question 2#
+function askQuestionP2() {
+  var userAnswer = prompt ("They could get sick, have to move, or die. ") ;
+  if (userAnswer == "What might happen to an animal in a polluted area?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionE2() {
+  var userAnswer = prompt (" This is the source of all energy on earth") ;
+  if (userAnswer == "What is the sun") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionCe2() {
+  var userAnswer = prompt ("Supports and protects") ;
+  if (userAnswer == "What does a cell wall in a plant do{") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionEn2() {
+  var userAnswer = prompt (" The ability to do work") ;
+  if (userAnswer == "What is energy") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionC2() {
+  var userAnswer = prompt ("Type of community that has separate houses ") ;
+  if (userAnswer == "What is a suburban community?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+//Questions 3#
+function askQuestionP3() {
+  var userAnswer = prompt ("ride a bike or walk instead. ") ;
+  if (userAnswer == "To reduce air pollution instead of riding in a car we can") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionE3() {
+  var userAnswer = prompt ("These are organisms that make their own food and are after the sun in a food chain.") ;
+  if (userAnswer == "What are producers") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionCe3() {
+  var userAnswer = prompt ("the material or protoplasm within a living cell, excluding the nucleus.") ;
+  if (userAnswer == "Eukaryotic Cells") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionEn3() {
+  var userAnswer = prompt ("Biomass, hydropower, geothermal, wind and solar.") ;
+  if (userAnswer == "What are renewable resources") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionC3() {
+  var userAnswer = prompt ("Something that you would see a lot of in a rural community") ;
+  if (userAnswer == "What are farm animals?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+//Questions 4#
+function askQuestionP4() {
+  var userAnswer = prompt ("https://trinket.io/library/trinkets/cc40cf2265When we throw something out the window of a car or leave unwanted items in an environment, we are") ;
+  if (userAnswer == "Cells") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionE4() {
+  var userAnswer = prompt ("The organism that eats the producer is called this.") ;
+  if (userAnswer == "Cells") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionCe4() {
+  var userAnswer = prompt ("Powerhouse of the cell") ;
+  if (userAnswer == "What is the mitocondria") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionEn4() {
+  var userAnswer = prompt ("You can burn it or you can eat it.") ;
+  if (userAnswer == "Chemical energy") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionC4() {
+  var userAnswer = prompt ("One kind of transportation used in an urban community") ;
+  if (userAnswer == "What is a taxi, subway or train?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+//Questions 5#
+function askQuestionP5() {
+  var userAnswer = prompt ("another chemical mixes with rain and flows into a large body of water it is called") ;
+  if (userAnswer == "Run-off") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionE5() {
+  var userAnswer = prompt ("Organism that eats the primary consumer.") ;
+  if (userAnswer == "What is a secondary consumer") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionCe5() {
+  var userAnswer = prompt ("Small storage spaces in the cytoplasm") ;
+  if (userAnswer == "What are the vacuoles?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionEn5() {
+  var userAnswer = prompt ("A material or an object that does not easily allow heat, electricity, light, or sound to pass through it.") ;
+  if (userAnswer == "Insulator") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
+function askQuestionC5() {
+  var userAnswer = prompt ("Type of community that has a lot of people and connected buildings") ;
+  if (userAnswer == "What is an urban community?") {
+    alert("Correct!");
+  }
+  else {
+    alert("Thats Not right!!!!");
+  }
+}
